2dff18703b9683f94f8d067c2a89b4071a80fa62
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-08-10 18:03:57 -0700

    stuff

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,66 @@ var router = express.Router();
 // Generate access token
 // Your code here
 
+router.post('/createchannel', function(req, res) {
+	var data = '';
+	req.on('data', function(d) {
+		data+=d;
+	});
+
+	req.on('end', function() {
+		console.log(data);
+	var name = data.name;
+	getToken(function(token) {
+		request.post({
+			url: 'http://notifications-stg.api.autodesk.com/notifications/v1/channel/',
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				body: {
+					"channelName" : name,
+					"channelType" : 'broadcast'
+				}
+			}),
+			headers: {
+				"Access-Control-Allow-Origin": '*', Authorization: "Bearer "+token 
+			}
+		}, function(err, res, body) {
+			console.log(err);
+			console.log(res);
+			console.log(body);
+		});
+	});
+	});
+});
+
+var getToken = function(callback) {
+	var text = "client_id="+credentials.credentials.client_id+"&client_secret="+credentials.credentials.client_secret+"&grant_type=client_credentials";
+	console.log(text);
+	request({
+	    url: 'https://developer-stg.api.autodesk.com/authentication/v1/authenticate',
+	    method: 'POST',
+	    headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+	    body: text
+		}, 
+		function(error, response, body) {
+      callback(JSON.parse(body).access_token);
+    });
+}
+
+router.get('/token', function(req, res) {
+	var text = "client_id="+credentials.credentials.client_id+"&client_secret="+credentials.credentials.client_secret+"&grant_type=client_credentials";
+	console.log(text);
+	request({
+	    url: 'https://developer-stg.api.autodesk.com/authentication/v1/authenticate',
+	    method: 'POST',
+	    headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+	    body: text
+		}, 
+		function(error, response, body) {
+      res.send(200, body);
+    });
+});
+
 
 
 module.exports = router;
--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,157 @@
-var defaultUrn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE1LTA3LTI3LTE3LTI5LTQ2LXlybGNod3FyYWV4ZTRveWFqdG52MzdqcXZjbXkvUm9ib3RBcm0xLmR3Zng=';
+
+var defaultUrn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE1LTA4LTExLTAwLTIxLTI2LWhyZG1vd2d3ejhpb3N0anVlbGR3c3gxaXZ6eW0vUm9ib3RBcm0uZHdmeA==';
 
 $(document).ready(function () {
-    // Your code here
-    
-    // Instantiate viewer factory
-    // Allow different urn to be passed as url parameter
+	getToken(function(token) {
+		console.log(token);
+	  var options = {
 
+	    'document' : 'urn:'+defaultUrn,
+
+	    'env':'AutodeskStaging',
+
+	    'getAccessToken': function() { return token },
+
+	    'refreshToken': function() { return token }
+
+	  };
+
+	  var viewerElement = document.getElementById('viewer');
+
+	  var viewer = new Autodesk.Viewing.Viewer3D(viewerElement, {});
+
+	  Autodesk.Viewing.Initializer(options,function() {
+
+	    viewer.initialize();
+	    createChannel('221sad', token);
+	    loadComments(token);
+	    loadDocument(viewer, options.document);
+
+	  });
+	});
 });
 
 function onError(error) {
     console.log('Error: ' + error);
 };
+
+
+function createChannel(name, token) {
+  $.ajax({
+    url: 'http://livereview.com:3000/api/createchannel',
+    type: 'POST',
+    data: JSON.stringify({'name' : name}),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(err) {
+      console.error(err);
+    },
+    complete: function() {
+    }
+  });
+}
+
+
+// This method returns a valid access token  For the Quick Start we are just returning the access token
+
+// we obtained in step 2.  In the real world, you would never do this.
+
+function getToken(callback) {
+
+  $.ajax({
+    url: 'http://livereview.com:3000/api/token',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data) {
+      callback(JSON.parse(data).access_token);
+    },
+    error: function(err) {
+      console.error(err);
+    },
+    complete: function() {
+    }
+  });
+
+}
+
+function postComment() {
+	var text = $('#commentText')[0].value;
+	getToken(function(token) {
+		$.ajax({
+		    url: 'https://developer-stg.api.autodesk.com/comments/v2/resources/'+defaultUrn,
+		    type: 'POST',
+		    data: JSON.stringify({
+		    	body: text
+		    }),
+		    contentType: 'plain/text',
+		    headers: {"Access-Control-Allow-Origin": '*', Authorization: "Bearer "+token },
+		    success: function(data) {
+		      console.log('loaded comments');
+		      // Don't bother if we have nothing to work with
+		      if (!data.results || !data.results.length) { return; }
+
+		      $('#comments').empty();
+
+		      for(var i = 0; i<data.results; i++) {
+		        var elem = $('<div></div>');
+		        elem.text = data.results[i];
+		        $('#comments').append(elem)
+		      }
+
+		    },
+		    error: function(err) {
+		      console.error(err);
+		    },
+		    complete: function() {
+		    }
+	  	});
+	  	loadComments(token);
+  });
+}
+
+function loadComments(token) {
+  $.ajax({
+    url: 'https://developer-stg.api.autodesk.com/comments/v2/resources/'+defaultUrn,
+    type: 'GET',
+    contentType: 'application/json',
+    headers: {"Access-Control-Allow-Origin": '*', Authorization: "Bearer "+token },
+    success: function(data) {
+      console.log('loaded comments');
+      // Don't bother if we have nothing to work with
+
+      $('#comments').empty();
+      console.log(data);
+      for(var i = 0; i<data.length; i++) {
+      	console.log('asdasd');
+        var elem = $('<div class="comment">'+data[i].body+'</div>');
+        $('#comments').append(elem);
+      }
+
+    },
+    error: function(err) {
+      console.error(err);
+    },
+    complete: function() {
+    }
+  });
+}
+
+
+function loadDocument(viewer, documentId) {
+    // Find the first 3d geometry and load that.
+    Autodesk.Viewing.Document.load(documentId, function(doc) {// onLoadCallback
+    var geometryItems = [];
+    geometryItems = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {
+        'type' : 'geometry',
+        'role' : '3d'
+    }, true);
+
+    if (geometryItems.length > 0) {
+        viewer.load(doc.getViewablePath(geometryItems[0]));
+    }
+ }, function(errorMsg) {// onErrorCallback
+    alert("Load Error: " + errorMsg);
+    });
+}
